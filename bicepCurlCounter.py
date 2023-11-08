1c22cd5de691b27fc415dc169b2234d33da06256
Adarsh Patel <swe.adarsh@gmail.com>
2023-11-07 19:39:32 -0500

    add pose detection

--- bicepCurlCounter.py
+++ bicepCurlCounter.py
@@ -1,5 +1,49 @@
+# https://www.youtube.com/watch?v=06TE_U21FK4
+
 import cv2
 import mediapipe as mp
 import numpy as np
 mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+mp_pose = mp.solutions.pose 
+
+
+cap = cv2.VideoCapture(0)
+# setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    # video feed
+    while(cap.isOpened()):
+        ret,frame = cap.read()
+
+        # recolor image
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # make detection
+        results = pose.process(image)
+        
+        # recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            print(landmarks)
+        except:
+            pass
+
+        # render detections
+        mp_drawing.draw_landmarks(image, 
+                                  results.pose_landmarks, 
+                                  mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), # joint
+                                  mp_drawing.DrawingSpec(color=(245,66,117), thickness=2, circle_radius=2)  # connections
+                                  )
+        
+        cv2.imshow("Mediapipe Feed", image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+            
+    cap.release()
+    cv2.destroyAllWindows()
